0958fe6f1ca2a7423dbf38596196802f2c6410ed
sm-rijal <srijal@binaracademy.org>
2024-01-23 10:06:38 +0700

    ch5day1

---
+++ hitung.js
@@ -0,0 +1,17 @@
+
+// function regular
+function hitung(a, b){
+
+    // kondisi
+    // looping
+
+    return a + b
+}
+
+// arrow function
+// const hitung = (a, b) => {
+//     return a + b
+// }
+
+module.exports = hitung
+
---
+++ say.js
@@ -0,0 +1,11 @@
+
+
+exports.say = () => {
+
+    return "Haiiii"
+}
+
+exports.kurang = (a, b) => {
+
+    return a - b
+}
