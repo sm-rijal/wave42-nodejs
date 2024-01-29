f0c1845710fd64d60fc2023ca37357eeafeedb7c
sm-rijal <srijal@binaracademy.org>
2024-01-29 19:56:10 +0700

    ch5day3

--- index.js
+++ index.js
@@ -1,42 +1,20 @@
-const colors = require('colors');
-const validator = require('validator');
-const lodash = require("lodash")
-const moment = require("moment") 
-
-const isEmail = validator.isEmail('foo@bar.com'); 
-
-// console.log(isEmail);
-if(isEmail){
-    console.log("format email benar");
-} else {
-    console.log("masukan format email");
-}
-
-console.log(colors.green("Hayyy")); // outputs green text
-console.log(lodash.capitalize("SAMSUL"));
+const http = require("http")
 
 
-const valid = require('validator');
-
-const checkEmail = (email) =>
-{
-    
-    if(valid.isEmail(email))
-    {
-        console.log("email da benar");
-    }
-    else
-    {
-        console.log("ini bkn email");
+const server = http.createServer((request, response) => {
+    // console.log(request.method);
+    // console.log(request.url);
+    if(request.url === "/users"){
+        response.write("Hallo users")
+        // SELECT * FROM blog
+        // INSERT INTO product 
+    } else {
+        response.write("Hallo World")
     }
 
-}
-checkEmail("mariana.djum@yahoo.com")
+    response.end()
+})
 
-moment.locale("id")
-const waktu = moment().format();
-console.log(waktu);
-console.log(moment().format('dddd, DD MMMM YYYY, h:mm:ss a')) // January 24th 2024, 8:52:21 pm
-console.log(moment().format('dddd')) // Wednesday
+const port = 3000
 
-console.log("haiii");
+server.listen(port, () => console.log(`server running on port ${port}`))
