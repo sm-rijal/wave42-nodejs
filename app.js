91ac6e11eb87b4a2fc7da66dd6a0033c6d6938d1
sm-rijal <srijal@binaracademy.org>
2024-02-07 22:00:30 +0700

    ch6day2

--- app.js
+++ app.js
@@ -1,10 +1,65 @@
-const express = require("express")
-const cors = require("cors");
-const router = require("./routes");
-const app = express()
-const port = 5000;
+const express = require("express");
+const app = express();
+const port = 3000
 
-app.use(cors())
-app.use(router)
+app.set('view engine', 'ejs') // set view engine
+app.use(express.static('public')) // untuk membaca file yang ada di folder public
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', (req, res) => {
+    const nama = "Samsul Rijal"
+
+    res.render('index', {nama})
+});
+
+app.get('/users', (req, res) => {
+    const user = {
+        nama: "Samsul Rijal",
+        alamat: "Jakarta"
+    }
+
+    res.render('user', {user})
+});
+
+app.get('/products',(req, res) => {
+    const products = [
+        {
+            nama: "Aqua",
+            harga: 5000
+        },
+        {
+            nama: "Cleo",
+            harga: 3000
+        },
+        {
+            nama: "Le Minerale",
+            harga: 4000
+        },
+    ]
+
+    res.render('product', {products})
+
+})
+
+app.get('/form',(req, res) => {
+
+    res.render('form')
+})
+app.post('/add-product',(req, res) => {
+
+    // console.log(req.body.nama);
+    // console.log(req.body.harga);
+
+    const body = {
+        nama: req.body.nama,
+        harga: req.body.harga
+    }
+    console.log(body);
+
+    res.redirect('form')
+})
+
+
+
 
 app.listen(port, () => console.log(`server running on port ${port}`))
