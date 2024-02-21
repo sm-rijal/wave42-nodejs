5992a5cbcbd1058a4af8fd6f2bc19927f21c6809
sm-rijal <srijal@binaracademy.org>
2024-02-21 16:08:41 +0700

    ch7day1

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
-const express = require("express");
-const router = require("./routes");
+const express = require('express');
 const app = express();
-const port = 8000;
 
-app.use(express.json());
-app.use('/api',router);
-// http//:localhost:8000/api
+app.set('view engine', 'ejs');
+app.use(express.static('public')) // untuk membaca file yang ada di folder public
+app.use(express.urlencoded({extended: false}))
 
-app.listen(port, () => console.log(`server running on port ${port}`));
+app.get('/', (req, res) => {
+
+    const halo = "Hallooow"
+    res.render('index', {halo})
+})
+
+let products = []
+
+app.get('/products', (req, res) => {
+
+    res.render('products', {products})
+
+})
+
+app.get('/form', (req, res) => {
+    res.render('form')
+})
+
+app.post('/add-product', (req, res) => {
+    products.push(req.body)
+
+    res.redirect('/products')
+})
+
+app.listen(8000)
