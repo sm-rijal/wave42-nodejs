87800d028e4ff07e6509d5d1f670b1706e3fa08d
sm-rijal <srijal@binaracademy.org>
2024-03-01 22:34:38 +0700

    ch7day6

--- models/productModel..js
+++ models/productModel..js
@@ -4,12 +4,12 @@ const knex = require('knex')(knexfile.development);
 
 
 const findAllProduct = () => {
-   return knex('products').select('products.id', 'products.name', 'products.price','store.name as toko')
+   return knex('products').select('products.id', 'products.name', 'products.price','store.name as toko', 'products.store_id')
     .innerJoin('store', 'products.store_id', '=', 'store.id').orderBy('id', 'desc')
 }
 
 const findByIdProduct = (id) => {
-    return knex('products').select('products.id', 'products.name', 'products.price','store.name as toko')
+    return knex('products').select('products.id', 'products.name', 'products.price','store.name as toko', 'products.store_id')
      .innerJoin('store', 'products.store_id', '=', 'store.id').where('products.id', id).first();
  }
 
